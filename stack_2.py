851714afd8daed5e481fa7f96e2e80f9e1fab728
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-09-12 00:27:57 +0300

    incorrect file names corrected, minor changes

--- stack_2.py
+++ stack_2.py
@@ -2,24 +2,17 @@ from test_data import TEST_1, TEST_2, TEST_3, TEST_4, TEST_5, TEST_6
 
 CLOSER = {'(': ')', '[': ']', '{': '}'}
 
-#print(PAIRS['{'])
 
 class Stack():
     """
-    Stack is an abstract class that realises a list of elements organized by LIFO. It works with balanced/unbalanced parentheses.
-    Balanced parentheses mean that each opening character has a matching closing character and that the parenthesis pairs are correctly nested.
+    Stack is an abstract class that realises a list of elements organized by LIFO.
     """
 
     def __init__(self, string: str):
         self.stack = []
         self.string = string
-        # self.string_splitted = list(self.string)
-        # self.pop_counter = 0 # коэф удаление пар скобок
-        #print('string_splitted: ', len(self.string_splitted), self.string_splitted)
-        if len(self.string) % 2 != 0:
-            print('Переданные данные НЕ сбалансированы! Дальнейшая проверка проверка прекращается.')
-        else:
-            print('Проверка четности пройдена успешно, анализируем содержание данных')
+        #self.string_splitted = list(self.string)
+
 
     def isEmpty(self):
         """ Boolean function that indicates empty status of stack """
@@ -36,7 +29,6 @@ class Stack():
     def pop(self):
         """ The function for deleting last element, returning previous element """
         self.stack.pop(len(self.stack) - 1)
-        #self.pop_counter += 1
         result = 0
         try:
             result = self.stack[len(self.stack) - 1]
@@ -46,87 +38,30 @@ class Stack():
 
     def peek(self):
         """ The function for getting last stack element """
-        result = 0
-        try:
+        if self.isEmpty():
+            result = None
+        else:
             result = self.stack[len(self.stack) - 1]
-        except:
-            IndexError, KeyError
         return result
 
+    #     size - возвращает количество элементов в стеке.
     def size(self):
-        """ The function returning the quantity of elements (stack size) """
+        """ The function returning the quntity of elements (stack size) """
         return len(self.stack)
 
     def isBalanced(self):
         """ The function is matching closing character and that the parenthesis pairs are correctly nested. Works with: (), [], {} only """
-        input = iter(self.string)
-        while True:
-            try:
-                element = next(input)
-            except:
-                StopIteration
+        for element in list(self.string):
             #print('element:', element)
             if element in CLOSER: #adding element to stack if it is opening
                 self.push(element)
             else: #if element is closing
                 if CLOSER[self.peek()] == element: #check is it fit for last stack element
                     self.pop()
-
+                else:
+                    print('Последовательность начинается с закрывающей скобки! Обработка прекращается!')
         if self.isEmpty():
             print(f'Cтрока {self.string} сбалансирована')
         else:
             print(f'Cтрока {self.string} не сбалансирована')
         print('Состав стэка: ', self.stack)
-
-
-
-
-
-
-
-
-
-
-        # ####old_version
-        # if self.isEmpty():
-        #     self.push(self.string[0])
-        # else:
-        #     pass
-        # for index in range(1, len(self.string)):
-        #     if CLOSER[self.peek()] == self.string[index]:
-        #         try:
-        #             self.pop()
-        #         except:
-        #             IndexError
-        #     else:
-        #         self.push(self.string[index])
-        #
-        # if self.isEmpty():
-        #     print(f'Cтрока {self.string} сбалансирована')
-        # else:
-        #     print(f'Cтрока {self.string} не сбалансирована')
-        # print('Состав стэка: ', self.stack)
-
-
-        # for index, element in enumerate(self.string_splitted):
-        #     print('***', self.size())
-        #     if index == 0:
-        #         self.stack.append(element)
-        #     else:
-        #         if element != CLOSER[self.stack[index - 1 - self.pop_counter]]:
-        #             self.stack.append(element)
-        #         else:
-        #             self.stack.pop()
-        #
-        # print('Стэк: ', self.stack)
-
-
-
-#     Используя стек из задания 1 необходимо решить задачу на проверку сбалансированности скобок. Сбалансированность скобок означает, что каждый открывающий символ имеет соответствующий ему закрывающий, и пары скобок правильно вложены друг в друга. Сбалансированными последовательности будут следующие скобки:
-#
-#     (((([{}]))))
-#     [([])((([[[]]])))]{()}
-#     {{[()]}} Несбалансированными последовательности:
-#     }{}
-#     {{[(])]}}
-#     [[{())}] Программа ожидаем на вход строку с скобками. На выход сообщение "Сбалансированно", если строка корректная и "Небалансированно", если строка составлена не верно.
--- starter.py
+++ starter.py
@@ -1,4 +1,4 @@
-from draft_2 import Stack
+from stack_2 import Stack
 from test_data import TEST_1, TEST_2, TEST_3, TEST_4, TEST_5, TEST_6
 
 NL = '\n'
